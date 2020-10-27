a4b4554a03b0a4fcf29ff003505c92f8008ecd34
provsalt <gooraymond39@gmail.com>
2020-10-27 20:33:24 +0800

    Fix invalid synatx for mysql

--- economy.go
+++ economy.go
@@ -30,7 +30,7 @@ func New(s *dragonfly.Server, path string, config string) (*Economy, error) {
 	db.SetMaxOpenConns(5)
 	db.SetMaxIdleConns(2)
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS " +
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS economy" +
 		"UUID VARCHAR(36) PRIMARY KEY," +
 		"username TEXT NOT NULL," +
 		"money FLOAT DEFAULT 0",

d5e451cd4b4e3df3db0a887c5fa81192463bbc21
provsalt <gooraymond39@gmail.com>
2020-10-31 17:31:35 +0800

    Add Init function for players

--- economy.go
+++ economy.go
@@ -2,6 +2,7 @@ package Economy
 
 import (
 	"database/sql"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/df-mc/dragonfly/dragonfly"
 	"github.com/df-mc/dragonfly/dragonfly/player"
 	_ "github.com/go-sql-driver/mysql"
@@ -16,29 +17,47 @@ type Economy struct {
 	config string
 }
 
-func New(s *dragonfly.Server, path string, config string) (*Economy, error) {
+var Db *sql.DB
+
+func New(s *dragonfly.Server, path string, config string) *Economy {
 	e := &Economy{
 		server: s,
 		path:   path,
 		config: config,
 	}
+	return e
+}
+
+func (e Economy) StartDB() (sql.Result, error) {
 	db, err := sql.Open("mysql", e.config)
 	if err != nil {
 		return nil, err
 	}
 	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(5)
-	db.SetMaxIdleConns(2)
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS economy ( UUID VARCHAR(36) PRIMARY KEY, username TEXT NOT NULL, money FLOAT DEFAULT 0);")
+	db.SetMaxOpenConns(50)
+	db.SetMaxIdleConns(30)
+	Db = db
+	res, err := db.Exec("CREATE TABLE IF NOT EXISTS economy ( XUID VARCHAR(36) PRIMARY KEY, username TEXT NOT NULL, money FLOAT DEFAULT 0);")
 	if err != nil {
 		return nil, err
 	}
-	return e, nil
+	return res, nil
 }
 
 func (e Economy) InitPlayer(player *player.Player, defaultmoney float64) error {
-	panic("TODO")
+	r, err := Db.Query("SELECT username FROM economy WHERE XUID=?", player.XUID())
+	if err != nil {
+		return err
+	}
+	if r == nil {
+		res, err := Db.Exec("REPLACE INTO economy (XUID, username, money) VALUES (?, ?, ?)", player.XUID(), player.Name(), defaultmoney)
+		if err != nil {
+			return err
+		}
+		spew.Dump(res)
+	}
+	spew.Dump(r)
+	return nil
 }
 
 func (e Economy) GetMoney(player *player.Player) error {

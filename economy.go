de501a755b90b07d5bd5270cd3d58399a6f9574c
provsalt <gooraymond39@gmail.com>
2022-03-13 09:44:53 +0800

    Add handlers

--- economy.go
+++ economy.go
@@ -1,13 +1,68 @@
 package economy
 
-import "github.com/provsalt/economy/provider"
+import (
+	"github.com/df-mc/dragonfly/dragonfly/event"
+	"github.com/provsalt/economy/handler"
+	"github.com/provsalt/economy/provider"
+)
 
 type Economy struct {
-	provider.Provider
+	p provider.Provider
+	h handler.EconomyHandler
 }
 
+// New creates a new economy instance with a provider.
 func New(p provider.Provider) *Economy {
 	return &Economy{
 		p,
+		handler.NopEconomyHandler{},
 	}
+}
+
+// Handle adds a new handler to handle economy changes.
+func (e *Economy) Handle(h handler.EconomyHandler) {
+	e.h = h
+}
+
+// Balance ...
+func (e *Economy) Balance(XUID string) (uint64, error) {
+	return e.p.Balance(XUID)
+}
+
+// Set ...
+func (e *Economy) Set(XUID string, amount uint64) error {
+	ctx := event.C()
+	e.h.HandleChange(ctx, XUID, handler.ChangeTypeSet, amount)
+	var err error
+	ctx.Continue(func() {
+		err = e.p.Set(XUID, amount)
+	})
+	return err
+}
+
+// Increase ...
+func (e *Economy) Increase(XUID string, amount uint64) error {
+	ctx := event.C()
+	e.h.HandleChange(ctx, XUID, handler.ChangeTypeIncrease, amount)
+	var err error
+	ctx.Continue(func() {
+		err = e.p.Increase(XUID, amount)
+	})
+	return err
+}
+
+// Reduce ...
+func (e *Economy) Reduce(XUID string, amount uint64) error {
+	ctx := event.C()
+	e.h.HandleChange(ctx, XUID, handler.ChangeTypeDecrease, amount)
+	var err error
+	ctx.Continue(func() {
+		err = e.p.Reduce(XUID, amount)
+	})
+	return err
+}
+
+// Close ...
+func (e *Economy) Close() error {
+	return e.p.Close()
 }
---
+++ handler/economyHandler.go
@@ -0,0 +1,19 @@
+package handler
+
+import "github.com/df-mc/dragonfly/dragonfly/event"
+
+// ChangeType is the type of change that has occurred.
+const (
+	ChangeTypeSet = iota
+	ChangeTypeIncrease
+	ChangeTypeDecrease
+)
+
+type EconomyHandler interface {
+	// HandleChange handles an economy change. The change is of the type ChangeType.
+	HandleChange(ctx *event.Context, XUID string, _type int, amount uint64)
+}
+
+type NopEconomyHandler struct{}
+
+func (n NopEconomyHandler) HandleChange(*event.Context, string, int, uint64) {}

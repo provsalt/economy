a65dcfd8a51997c2a9f5b5dd7b612e9dcd10a97a
provsalt <gooraymond39@gmail.com>
2022-03-26 21:00:31 +0800

    provider/provider.go Add io.closer to provider interface

--- provider/provider.go
+++ provider/provider.go
@@ -1,7 +1,11 @@
 package provider
 
+import "io"
+
 // Provider is an interface that defines the methods that a provider must implement.
 type Provider interface {
+	io.Closer
+
 	Balance(UUID string) (uint64, error)
 
 	Set(UUID string, value uint64) error
